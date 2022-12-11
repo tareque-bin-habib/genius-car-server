70cd4a178796dce6a6c9dd47ee3d074af8709f08
tareque-bin-habib <shad.tbh@gmail.com>
2022-12-11 21:40:51 +0600

    get data from DB

--- index.js
+++ index.js
@@ -13,12 +13,25 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wzwjy79.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+
+async function run() {
+    try {
+        const serviceCollection = client.db('geniusCar').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(error => console.error(error))
 
 
 
